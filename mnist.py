a34827042adbaf30c2130c47a306c22c4b3d46b3
dhruv <heydhruvpatel@gmail.com>
2021-06-27 15:13:30 -0500

    tiny changes

--- mnist.py
+++ mnist.py
@@ -18,11 +18,11 @@ writer = SummaryWriter()
 
 
 model_name = 'efficientnet-b0'
-BATCH_SIZE = 16
+BATCH_SIZE = 8
 SEED = 44
 EPOCHS = 4
 lr_rate = 0.001
-img_size = 32  
+img_size = 32
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -48,7 +48,7 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 
 model = EfficientNet.from_name(model_name)
 
-image_size = EfficientNet.get_image_size(model_name)        
+image_size = EfficientNet.get_image_size(model_name)
 print(f'\n\nalthough b0 input img size is {image_size}; you can get away with a resize of {img_size}. \n')
 
 # adjust the final linear layer in and out features.
@@ -90,6 +90,7 @@ for epoch in range(EPOCHS):
                   .format(epoch + 1, EPOCHS, i + 1, total_step, loss.item()))
 
 train_time = time.time() - start_time
+print('\n'*5)
 print('*'*25)
 print(f'\n\n Time taken to train the model :: {train_time/60:.2f} minutes.')
 print('*'*25)
@@ -112,5 +113,11 @@ with torch.no_grad():
 
     print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
 
-PATH = "mnist.pt"
-torch.save(model, PATH)
+
+
+PATH = 'saved_models/'
+MODEL = 'mnist.pt'
+if not os.path.isdir(PATH):
+    os.mkdir(PATH) 
+
+torch.save(model, PATH+MODEL)
